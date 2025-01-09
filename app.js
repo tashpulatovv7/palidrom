aee900001e97d39c06133ddeaad58d9945997415
tashpulatovv7 <tashpulatovvbiolbek@gmail.com>
2025-01-10 02:09:33 +0500

    finish

--- app.js
+++ app.js
@@ -1,12 +1,39 @@
-function fibonacci(n) {
-	if (n <= 1) {
-		return n;
-	} else {
-		return fibonacci(n - 1) + fibonacci(n - 2);
-	}
-}
+// 4
 
-let n = 7;
-for (let i = 0; i < n; i++) {
-	console.log(`Fibonacci(${i}) = ${fibonacci(i)}`);
-}
+// function fibonacci(n) {
+// 	if (n <= 1) {
+// 		return n;
+// 	} else {
+// 		return fibonacci(n - 1) + fibonacci(n - 2);
+// 	}
+// }
+
+// let n = 7;
+// for (let i = 0; i < n; i++) {
+// 	console.log(`Fibonacci(${i}) = ${fibonacci(i)}`);
+// }
+
+// 1
+
+// function findPairs(arr, target) {
+// 	let pairs = [];
+// 	let seen = new Set();
+
+// 	for (let i = 0; i < arr.length; i++) {
+// 		let complement = target - arr[i];
+
+// 		if (seen.has(complement)) {
+// 			pairs.push([arr[i], complement]);
+// 		}
+
+// 		seen.add(arr[i]);
+// 	}
+
+// 	return pairs;
+// }
+
+// let arr = [10, 15, 3, 7, 5, 1, 9];
+// let target = 10;
+
+// let result = findPairs(arr, target);
+// console.log(result);
